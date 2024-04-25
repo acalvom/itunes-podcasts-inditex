5933221086d8b6f8c9b409e12bb19d73f3be3473
acalvom <andreac.3108@gmail.com>
2024-04-25 09:58:17 +0200

    feat: get podcasts from api

--- src/models/podcasts/infrastructure/api-podcast.respository.ts
+++ src/models/podcasts/infrastructure/api-podcast.respository.ts
@@ -1,13 +1,14 @@
 import axios from 'axios'
 import { Podcast } from '../domain/podcast'
 import { PodcastRepository } from '../domain/podcast.repository'
+import { AxiosResponse } from './dtos/podcast-api-response.dto'
+import { apiResponseToPrimitives } from './mappers/podcasts.mapper'
 
 export class ApiPodcastRepository implements PodcastRepository {
   async getAll(): Promise<Podcast[]> {
     // TODO: add lib with http endpoints
-    // TODO: lo que recibo del api no coincide con lo que tengo en IPodcastPrimitives
-    const res = await axios.get(`${import.meta.env.VITE_API_URL}`)
-    const rawPodcasts = res.data
-    return rawPodcasts
+    const rawResponse: AxiosResponse = await axios.get(`${import.meta.env.VITE_API_URL}`)
+    const jsonPodcasts = apiResponseToPrimitives(rawResponse.data)
+    return jsonPodcasts.map(Podcast.fromPrimitives)
   }
 }
