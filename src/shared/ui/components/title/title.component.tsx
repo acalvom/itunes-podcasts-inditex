3a7ad2e5f83d13ef6e20b0751dc50d95bd8c35aa
acalvom <andreac.3108@gmail.com>
2024-04-30 15:20:35 +0200

    style: fix styles

--- src/shared/ui/components/title/title.component.tsx
+++ src/shared/ui/components/title/title.component.tsx
@@ -1,6 +1,6 @@
 export const Title = ({ title }: { title: string }) => {
   return (
-    <div className="w-full rounded-sm align-middle shadow-xl xl:mx-6">
+    <div className="w-full rounded-sm align-middle shadow-xl ">
       <h3 className="px-4 py-6 text-2xl font-bold">{title}</h3>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ export default {
         'blue-main': '#41729F',
         'blue-dark': '#274472',
         'blue-light': '#C3E0E5',
+        'grey-main': '#F2F1F0',
+        'grey-light': '#F9F9FB',
       },
       gridTemplateColumns: {
         '60/20/15': '60% 20% 15%',
