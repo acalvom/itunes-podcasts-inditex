866e88b812ffb251d51fb8d3adab4ba81964ee02
acalvom <andreac.3108@gmail.com>
2024-04-30 11:11:53 +0200

    style: match styles

--- src/models/podcasts/ui/components/episode/episode-detail-card.component.tsx
+++ src/models/podcasts/ui/components/episode/episode-detail-card.component.tsx
@@ -15,7 +15,7 @@ export const EpisodeDetailCard = ({ episodeId, podcastId }: EpisodeDetailCardPro
       {episode && (
         <div className="flex h-min flex-col gap-2 divide-y rounded-sm px-4 py-8 shadow-xl">
           <p className="mb-2 text-xl font-bold">{episode.name}</p>
-          <p className="py-2 italic">{episode.description}</p>
+          <p className="py-2 text-sm font-light italic">{episode.description}</p>
           <audio src={episode.url} controls className="w-full pt-4"></audio>
         </div>
       )}
