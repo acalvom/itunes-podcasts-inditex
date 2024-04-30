cc89ad5fe44e444a4a014d5ad43b818101765579
acalvom <andreac.3108@gmail.com>
2024-04-30 08:32:15 +0200

    feat: control undefined episode

--- src/models/podcasts/ui/components/episode/episode-detail-card.component.tsx
+++ src/models/podcasts/ui/components/episode/episode-detail-card.component.tsx
@@ -1,3 +1,4 @@
+import { Loading } from '@/shared/ui/components/loading/loading.component'
 import { useEpisode } from '../../controllers/use-episode-by-id-hook'
 
 interface EpisodeDetailCardProps {
@@ -6,14 +7,18 @@ interface EpisodeDetailCardProps {
 }
 
 export const EpisodeDetailCard = ({ episodeId, podcastId }: EpisodeDetailCardProps) => {
-  const { episode } = useEpisode(episodeId, podcastId)
+  const { episode, isFetching } = useEpisode(episodeId, podcastId)
 
-  // TODO: control undefined
   return (
-    <div className="flex h-min flex-col gap-2 divide-y rounded-sm px-4 py-8 shadow-xl">
-      <p className="mb-2 text-xl font-bold">{episode?.name}</p>
-      <p className="py-2 italic">{episode?.description}</p>
-      <audio src={episode?.url} controls className="w-full pt-4"></audio>
-    </div>
+    <>
+      {isFetching && <Loading />}
+      {episode && (
+        <div className="flex h-min flex-col gap-2 divide-y rounded-sm px-4 py-8 shadow-xl">
+          <p className="mb-2 text-xl font-bold">{episode.name}</p>
+          <p className="py-2 italic">{episode.description}</p>
+          <audio src={episode.url} controls className="w-full pt-4"></audio>
+        </div>
+      )}
+    </>
   )
 }
