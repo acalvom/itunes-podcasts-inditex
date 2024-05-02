ae2c365b25beb15a5100723f2c0ecd3efc2c1de4
acalvom <andreac.3108@gmail.com>
2024-05-02 14:52:42 +0200

    test: add episode detail test

---
+++ cypress/e2e/usecases/get-episode-detail.cy.ts
@@ -0,0 +1,34 @@
+import { mockEpisode } from '../mocks/episode'
+import { mockPodcast } from '../mocks/podcast'
+
+describe('Get episode detail workflow', () => {
+  const appUrl = `${Cypress.env('appUrl')}`
+  const apiUrl = `${Cypress.env('apiUrl')}`
+
+  it('Click on an episode from the list', () => {
+    cy.intercept(
+      `${apiUrl}/lookup?id=${mockPodcast.id}&media=podcast&entity=podcastEpisode&limit=1000`,
+      { fixture: 'episodes-from-api.json' }
+    ).as('getEpisodesFromApi')
+
+    cy.visit(appUrl + '/podcast/' + mockPodcast.id + '/episode/' + mockEpisode.id)
+    cy.wait('@getEpisodesFromApi')
+
+    cy.getByTestId('episode-name').should('have.text', mockEpisode.name)
+    cy.getByTestId('episode-description').should('have.text', mockEpisode.description)
+    cy.getByTestId('episode-audio').should('have.attr', 'src').and('be.eq', mockEpisode.url)
+    cy.getByTestId('episode-audio')
+      .invoke('attr', 'src')
+      .then((audiofile) => {
+        const audio = new Audio(audiofile)
+        audio.play()
+      })
+    cy.getByTestId('podcast-name').parent().click()
+    cy.url().should('eq', appUrl + '/podcast/' + mockPodcast.id)
+  })
+
+  it('Back to podcast detail', () => {
+    cy.getByTestId('podcast-name').parent().click()
+    cy.url().should('eq', appUrl + '/podcast/' + mockPodcast.id)
+  })
+})
--- src/models/podcasts/ui/components/episode/episode-detail-card.component.tsx
+++ src/models/podcasts/ui/components/episode/episode-detail-card.component.tsx
@@ -14,9 +14,18 @@ export const EpisodeDetailCard = ({ episodeId, podcastId }: EpisodeDetailCardPro
       {isFetching && <Loading />}
       {episode && (
         <div className="flex h-min flex-col gap-2 divide-y rounded-sm px-4 py-8 shadow-xl">
-          <p className="mb-2 text-xl font-bold">{episode.name}</p>
-          <p className="py-2 text-sm font-light italic">{episode.description}</p>
-          <audio src={episode.url} controls className="w-full pt-4"></audio>
+          <p className="mb-2 text-xl font-bold" data-testid="episode-name">
+            {episode.name}
+          </p>
+          <p className="py-2 text-sm font-light italic" data-testid="episode-description">
+            {episode.description}
+          </p>
+          <audio
+            src={episode.url}
+            controls
+            className="w-full pt-4"
+            data-testid="episode-audio"
+          ></audio>
         </div>
       )}
     </>
