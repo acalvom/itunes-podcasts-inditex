33dde0d9bcd243c1967ca47411377ef3be003448
acalvom <andreac.3108@gmail.com>
2024-05-02 13:38:27 +0200

    test: add config for cypress

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -3,3 +3,9 @@
 Cypress.Commands.add('getByTestId', (testId) => {
   return cy.get(`[data-testid=${testId}]`)
 })
+
+Cypress.on('uncaught:exception', (err) => {
+  if (err.message.includes('Request failed with status code 400')) {
+    return false
+  }
+})
